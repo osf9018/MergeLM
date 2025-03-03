84dca9e1a51b26cb512bd11ea2736090a9b696c0
Olivier Ferret <10862540+osf9018@users.noreply.github.com>
2025-03-03 10:55:07 +0100

    model_merging_methods/merging_methods.py -- bug fix: when heads are merged, create a new head from the first merged model before the update of the merged model

--- model_merging_methods/merging_methods.py
+++ model_merging_methods/merging_methods.py
@@ -655,11 +655,7 @@ class MergingMethod:
         if merge_head_flag:
             head_merged_params = self.head_average_merging(models_to_merge)
             merged_params.update(head_merged_params)
+            merged_model.classifier = copy.deepcopy(models_to_merge[0].classifier)
         self.copy_params_to_model(params=merged_params, model=merged_model)
-        if merge_head_flag:
-            with torch.no_grad():
-                merged_model.classifier = copy.deepcopy(models_to_merge[0].classifier)
-                merged_model.classifier.weight.data.copy_(merged_params['classifier.weight'])
-                merged_model.classifier.bias.data.copy_(merged_params['classifier.bias'])
 
         return merged_model
